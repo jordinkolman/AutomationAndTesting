f157f974417eec70dae75f5bae86735df99ac813
Jordin Kolman <jordinkolman@gmail.com>
2019-12-22 21:15:53 -0600

    added some basic browser interaction

--- automation.py
+++ automation.py
@@ -4,3 +4,7 @@ chrome_browser = webdriver.Chrome('./chromedriver')
 
 chrome_browser.maximize_window()
 chrome_browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
+
+assert 'Selenium Easy Demo' in chrome_browser.title
+button = chrome_browser.find_element_by_class_name('btn-default')
+print(button.get_attribute('innerHTML'))
