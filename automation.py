3cd118b36059b3d77a6e1f55b8b76e3ab208e5fd
Jordin Kolman <jordinkolman@gmail.com>
2019-12-22 21:23:28 -0600

    added basic browser testing

--- automation.py
+++ automation.py
@@ -6,5 +6,19 @@ chrome_browser.maximize_window()
 chrome_browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
 
 assert 'Selenium Easy Demo' in chrome_browser.title
-button = chrome_browser.find_element_by_class_name('btn-default')
-print(button.get_attribute('innerHTML'))
+show_message_button = chrome_browser.find_element_by_class_name('btn-default')
+#print(show_message_button.get_attribute('innerHTML'))
+
+assert 'Show Message' in chrome_browser.page_source
+
+user_message = chrome_browser.find_element_by_id('user-message')
+user_button2 = chrome_browser.find_element_by_css_selector('#get-input > .btn')
+print(user_button2)
+user_message.clear()
+user_message.send_keys('Browser Input Typing Test')
+
+show_message_button.click()
+
+ouput_message = chrome_browser.find_element_by_id('display')
+assert 'Browser Input Typing Test' in ouput_message.text
+print(ouput_message.text)
